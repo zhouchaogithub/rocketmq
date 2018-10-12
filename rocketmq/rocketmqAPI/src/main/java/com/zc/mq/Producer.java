e9105610327f49183d83762b0c65199b0deee21e
zhouchao <zhouchaoit@sina.com>
2018-10-12 17:13:26 +0800

    生产者发送消息超时重试

--- rocketmq/rocketmqAPI/src/main/java/com/zc/mq/Producer.java
+++ rocketmq/rocketmqAPI/src/main/java/com/zc/mq/Producer.java
@@ -14,11 +14,14 @@ public class Producer {
     public static void main(String[] args) throws MQClientException, InterruptedException {
         DefaultMQProducer producer = new DefaultMQProducer("quickStart_producer");
         producer.setNamesrvAddr("192.168.126.102:9876;192.168.126.103:9876");
+        //设置失败重发次数（这设置5次）
+        producer.setRetryTimesWhenSendFailed(5);
         producer.start();
         for (int i=0;i<100;i++){
             try {
                 Message message = new Message("TopicQuickStart", "TagB", ("Hello RocketMq" + i).getBytes());
-                SendResult sendResult = producer.send(message);
+               //设置发送消息超时时间单位毫秒(当前设置1秒)
+                SendResult sendResult = producer.send(message,1000);
                 System.out.println("sendResult:========"+sendResult);
             }catch (Exception e){
                 e.printStackTrace();
